536f579565860282406f98d8acbeca9541a2f236
diamko <kikdiam@gmail.com>
2026-03-14 17:12:40 +0300

    Of re

--- app/src/main/java/ru/diamko/paleta/ui/auth/LoginScreen.kt
+++ app/src/main/java/ru/diamko/paleta/ui/auth/LoginScreen.kt
@@ -1,5 +1,6 @@
 package ru.diamko.paleta.ui.auth
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -19,9 +21,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import ru.diamko.paleta.R
@@ -56,8 +61,16 @@ fun LoginScreen(
                 verticalArrangement = Arrangement.spacedBy(16.dp),
             ) {
                 Spacer(modifier = Modifier.height(6.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.paleta_logo),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(88.dp)
+                        .align(Alignment.CenterHorizontally),
+                )
                 Text(
                     text = stringResource(id = R.string.app_name),
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
                     style = MaterialTheme.typography.displayMedium.copy(
                         brush = Brush.horizontalGradient(
                             colors = listOf(
@@ -66,6 +79,7 @@ fun LoginScreen(
                             ),
                         ),
                     ),
+                    textAlign = TextAlign.Center,
                 )
 
                 PaletaCard(modifier = Modifier.fillMaxWidth()) {
